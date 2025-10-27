79e88c9969de00c8e9dc464c9acadc079c14d5ab
ronnieluyo25 <ronnie.luyo25@gmail.com>
2025-10-27 16:59:23 -0500

    Add files via upload

--- api/_db.js
+++ api/_db.js
@@ -4,7 +4,7 @@ let _pool;
 function getPool() {
   if (!_pool) {
     const cs = process.env.NEON_DATABASE_URL;
-    if (!cs) throw new Error('NEON_DATABASE_URL not set');
+    if (!cs) throw new Error('NEON_DATABASE_URL2 not set');
 
     _pool = new Pool({
       connectionString: cs,
